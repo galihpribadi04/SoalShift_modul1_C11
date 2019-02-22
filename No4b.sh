5060cdd3fd7c90a2905e275af50a8eb5e9e5d7a7
IsnainiNurul <36990780+IsnainiNurul@users.noreply.github.com>
2019-02-22 17:37:50 +0700

    Create No4b.sh

---
+++ No4b.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+A=ABCDEFGHIJKLMNOPQRSTUVWXYZ
+B=abcdefghijklmnopqrstuvwxyz
+
+A2=($(echo ${A[@]})$(echo ${A[@]}))
+B2=($(echo ${B[@]})$(echo ${B[@]}))
+hour=`date +"%H"`
+rot=$hour
+
+simpan=($(echo ${A[@]})$(echo ${B[@]}))
+newA=$(echo $A | tr "${A:0:26}" "${A2:${rot}:26}")
+newB=$(echo $B | tr "${B:0:26}" "${B2:${rot}:26}")
+simpan2=($(echo ${newA[@]})$(echo ${newB[@]}))
+
+hasil=`date +"%H:%M %d-%m-%Y"`
+
+< /var/log/syslog > "$hasil" tr "$simpan" "$simpan2"
+
+
+
+
+
+
+
